c06f386326e9f92e064236ebd7c701dd4b7fe49e
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-13 17:57:47 +0300

    feat: implement constuctor

---
+++ ElectronicLibrary/ElectronicLibraryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ElectronicLibrary
+{
+    public class ElectronicLibraryService
+    {
+        private readonly string connectionString;
+        private readonly SqlConnection sqlConnection;
+
+        public ElectronicLibraryService(string connectionString)
+        {
+            ValidateConnectionString(connectionString);
+            this.connectionString = connectionString;
+            this.sqlConnection = new SqlConnection(connectionString);
+        }
+
+        private static void ValidateConnectionString(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString), "Connection string can't be null, empty or a whitespace.");
+            }
+        }
+    }
+}

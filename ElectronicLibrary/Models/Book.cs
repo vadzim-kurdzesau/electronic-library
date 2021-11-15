16e76b6168629b592ec0a0e33e61375b528566d7
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-16 01:12:17 +0300

    feat: implement Reader and Book models

---
+++ ElectronicLibrary/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ElectronicLibrary.Models
+{
+    public class Book
+    {
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+    }
+}

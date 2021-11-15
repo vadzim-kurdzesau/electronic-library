1d7f9da18bca1256f000f84098eb0b95d3fc0daf
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-15 21:14:30 +0300

    feat: move appsettings.json file higher in repository; add methods to get path to ConfigurationManager

--- ElectonicLibrary.Demo/ConfigurationManager.cs
+++ ElectonicLibrary.Demo/ConfigurationManager.cs
@@ -6,11 +6,15 @@ namespace ElectronicLibrary.Demo
     public class ConfigurationManager
     {
         public ConfigurationManager()
-        {
-            this.Configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            => this.Configuration = new ConfigurationBuilder().SetBasePath(GetPathToConfigurationFile())
                 .AddJsonFile("appsettings.json").Build();
-        }
 
         public IConfigurationRoot Configuration { get; }
+
+        private static string GetPathToConfigurationFile()
+            => GetPathToHigherDirectory(GetPathToHigherDirectory((Directory.GetCurrentDirectory())));
+
+        private static string GetPathToHigherDirectory(string path)
+            => Directory.GetParent(path).Parent.FullName;
     }
 }

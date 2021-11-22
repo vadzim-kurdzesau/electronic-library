0c98e4e2a10c7a20a9a2c1a04801f697a2d53d9a
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-22 12:40:14 +0300

    fix: fix books tests

--- ElectronicLibrary.Tests/BooksRepositoryTests.cs
+++ ElectronicLibrary.Tests/BooksRepositoryTests.cs
@@ -3,47 +3,18 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using ElectronicLibrary.Models;
+using ElectronicLibrary.Tests.TestData;
 using NUnit.Framework;
 
 namespace ElectronicLibrary.Tests
 {
     [TestFixture]
-    [Ignore("Needs Rework")]
+    //[Ignore("Needs Rework")]
     public class BooksRepositoryTests
     {
         // TODO: implement Book comparator
 
         private readonly ElectronicLibraryService library;
-
-        public static IEnumerable<TestCaseData> Books
-        {
-            get
-            {
-                yield return new TestCaseData(new Book()
-                {
-                    Id = 1,
-                    Name = "Pride and Prejudice",
-                    Author = "Jane Ousten",
-                    PublicationDate = new DateTime(1813, 1, 28)
-                });
-
-                yield return new TestCaseData(new Book()
-                {
-                    Id = 2,
-                    Name = "War and Peace",
-                    Author = "Leo Tolstoy",
-                    PublicationDate = new DateTime(1869, 1, 1)
-                });
-
-                yield return new TestCaseData(new Book()
-                {
-                    Id = 3,
-                    Name = "Crime and Punishment",
-                    Author = "Fyodor Dostoevsky",
-                    PublicationDate = new DateTime(1866, 1, 1)
-                });
-            }
-        }
 
         public BooksRepositoryTests()
         {
@@ -59,7 +30,7 @@ namespace ElectronicLibrary.Tests
         }
 
         [Order(0)]
-        [TestCaseSource(nameof(Books))]
+        [TestCaseSource(typeof(Books), nameof(Books.GetList))]
         public void BooksRepositoryTests_InsertBook(Book book)
         {
             this.library.BooksRepository.InsertBook(book);
@@ -70,9 +41,14 @@ namespace ElectronicLibrary.Tests
         [Test]
         public void BooksRepositoryTests_InsertInventoryNumber()
         {
-            for (int i = 1; i <= Books.Count(); i++)
+            for (int i = 1; i <= Books.GetList().Count(); i++)
             {
-                this.library.BooksRepository.InsertInventoryNumber(new InventoryNumber() { Id = i, BookId = i, Number = i.ToString() });
+                this.library.BooksRepository.InsertInventoryNumber(new InventoryNumber()
+                {
+                    Id = i,
+                    BookId = i,
+                    Number = i.ToString()
+                });
             }
 
             Assert.Pass();
@@ -82,7 +58,7 @@ namespace ElectronicLibrary.Tests
         [Test]
         public void BooksRepositoryTests_FindBookByName()
         {
-            foreach (var testData in Books)
+            foreach (var testData in Books.GetList())
             {
                 var expected = testData.Arguments[0] as Book;
                 var actual = this.library.BooksRepository.FindBooksByName(expected.Name).FirstOrDefault();
@@ -95,7 +71,7 @@ namespace ElectronicLibrary.Tests
         [Test]
         public void BooksRepositoryTests_FindBookByAuthor()
         {
-            foreach (var testData in Books)
+            foreach (var testData in Books.GetList())
             {
                 var expected = testData.Arguments[0] as Book;
                 var actual = this.library.BooksRepository.FindBooksByAuthor(expected.Author).FirstOrDefault();
@@ -108,7 +84,7 @@ namespace ElectronicLibrary.Tests
         [Test]
         public void BooksRepositoryTests_GetBook()
         {
-            foreach (var testData in Books)
+            foreach (var testData in Books.GetList())
             {
                 var expected = testData.Arguments[0] as Book;
                 var actual = this.library.BooksRepository.GetBook(expected.Id);
@@ -121,7 +97,7 @@ namespace ElectronicLibrary.Tests
         [Test]
         public void BooksRepositoryTests_GetInventoryNumbers()
         {
-            foreach (var testData in Books)
+            foreach (var testData in Books.GetList())
             {
                 var expected = testData.Arguments[0] as Book;
                 var actual = this.library.BooksRepository.GetInventoryNumbers(expected).First();
@@ -147,7 +123,7 @@ namespace ElectronicLibrary.Tests
         [Test]
         public void BooksRepositoryTests_UpdateBook()
         {
-            var expected = Books.First().Arguments[0] as Book;
+            var expected = Books.GetList().First().Arguments[0] as Book;
             expected.Author = "Jane Austen";
 
             this.library.BooksRepository.UpdateBook(expected);
@@ -158,7 +134,7 @@ namespace ElectronicLibrary.Tests
         [Test]
         public void BooksRepositoryTests_DeleteBook()
         {
-            for (int i = 1; i <= Books.Count(); i++)
+            for (int i = 1; i <= Books.GetList().Count(); i++)
             {
                 this.library.BooksRepository.DeleteBook(i);
             }
@@ -169,7 +145,7 @@ namespace ElectronicLibrary.Tests
         private static IEnumerable<Book> GetExpectedBooks()
         {
             List<Book> expected = new List<Book>();
-            foreach (var testData in Books.ToArray())
+            foreach (var testData in Books.GetList().ToArray())
             {
                 expected.Add(testData.Arguments[0] as Book);
             }

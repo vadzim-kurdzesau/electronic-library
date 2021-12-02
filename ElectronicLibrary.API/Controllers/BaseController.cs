89509eadbba0c35b855a4ec9a789b2e11ec83851
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-12-02 18:31:03 +0300

    feat: implement BaseController and CitiesController classes

---
+++ ElectronicLibrary.API/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicLibrary.API.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected readonly ElectronicLibraryService _electronicLibraryService;
+
+        protected BaseController(ElectronicLibraryService electronicLibraryService)
+        {
+            _electronicLibraryService = electronicLibraryService;
+        }
+    }
+}
---
+++ ElectronicLibrary.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectronicLibrary.API.Controllers
+{
+    [Route("[controller]")]
+    public class CitiesController : BaseController
+    {
+        public CitiesController(ElectronicLibraryService electronicLibraryService) : base(electronicLibraryService)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCities()
+        {
+            var cities = this._electronicLibraryService.GetAllCities;
+
+            return Ok(cities);
+        }
+    }
+}
